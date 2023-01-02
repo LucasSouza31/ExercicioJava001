05710e202ef5fe6e4c4ad60b59d777e8f223b7f9
lucassouza31 <lucaseasy10@gmail.com>
2023-01-02 19:19:57 -0300

    Praticando arrays

---
+++ Principal.java
@@ -0,0 +1,101 @@
+package youtube;
+
+import java.util.Scanner;
+
+public class Principal {
+
+        static int valorReferencia = 5;
+        static Produto produtos[]= new Produto[valorReferencia];
+        // alterações foram feitas, declarando a variável "ler" globalmente, por conta de erros "NoSuchElementException"
+        static Scanner ler = new Scanner(System.in); 
+        
+
+        private static void menu(){
+            System.out.println("BEM VINDO");
+            System.out.println("1-REALIZAR COMPRA");
+            System.out.println("2-ADICIONAR PRODUTOS AO ESTOQUE");
+            System.out.println("3-EXIBIR RELATÓRIO DO ESTOQUE");
+            System.out.println("4- SAIR");
+            
+            int selecionar= ler.nextInt();
+            switch (selecionar) {
+                case 1: comprarProduto(); break;
+                case 2: abastecerOEstoque(); break;
+                case 3: exibirEstoque(); break;
+            
+                default:
+                    break;
+            }
+           
+        }
+
+    private static void comprarProduto() {
+                
+        
+                int codigoProduto;
+                int quantidadeDesejada;
+                double valorTotal;
+        
+        for(int i=0;i<3;i++){
+            System.out.println("Nome: "+produtos[i].getNome()+" Código: "+i);
+        }   
+
+        System.out.println("Informar o códido do produto desejado: ");
+        codigoProduto=ler.nextInt();
+        System.out.println("Informar a quantidade desejada: ");
+        quantidadeDesejada= ler.nextInt();
+
+        valorTotal=produtos[codigoProduto].getValor()*quantidadeDesejada;
+        System.out.println("Valor total: "+valorTotal);
+        produtos[codigoProduto].retirarDoEstoque(quantidadeDesejada);
+
+        
+        menu();
+        }
+
+
+
+
+    private static void abastecerOEstoque() {
+                
+                for(int i=0;i<3;i++){
+                    System.out.println("Nome: "+produtos[i].getNome()+" Código: "+i);
+                }              
+
+                System.out.println("Informe o código do produto: ");
+                int codigoProduto= ler.nextInt();
+                System.out.println("Informe a quantidade que deseja adicionar: ");
+                int quantidadeParaAdicionar= ler.nextInt();
+                produtos[codigoProduto].adicionarAoEstoque(quantidadeParaAdicionar);
+                
+                menu();
+        }
+
+    private static void exibirEstoque() {
+            for(int i=0;i<3;i++){
+                System.out.println("Código do produto: "+i);
+                produtos[i].relatorioDoEstoque();
+                System.out.println("__________________");
+            }
+            menu();
+        }
+    
+
+
+
+
+    public static void main(String[] args) {
+
+        Produto p1= new Produto("Coca-Cola", 100, 9.9);
+        Produto p2= new Produto("Guaraná", 100, 8.5);
+        Produto p3= new Produto("Vokda", 100, 39.99);
+
+        produtos[0]=p1;
+        produtos[1]=p2;
+        produtos[2]=p3;
+
+        menu();
+
+
+    }
+}
---
+++ Produto.java
@@ -0,0 +1,53 @@
+package youtube;
+
+public class Produto {
+    private String nome;
+    private int quantidadeEmEstoque;
+    private double valor;
+
+
+    public Produto(String nome, int quantidadeEmEstoque, double valor) {
+        this.nome = nome;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.valor = valor;
+    }
+
+    
+    public void adicionarAoEstoque(int quantidadeParaAdicionar) {
+        this.quantidadeEmEstoque+=quantidadeParaAdicionar;
+    }
+
+    public void retirarDoEstoque(int quantidadeRetirada) {
+        this.quantidadeEmEstoque-=quantidadeRetirada;
+    }
+
+    public void relatorioDoEstoque() {
+        System.out.println("Nome: "+getNome());
+        System.out.println("Quantidade: "+getQuantidadeEmEstoque());
+        System.out.println("Valor unitário: "+getValor());
+    }
+
+    // getters and setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+
+
+}
